7339e6353d5765cd13686cb715077779014baa14
Vitor Parras <vitorhugoparras@hotmail.com>
2022-07-14 19:26:54 -0300

    Add project files.

---
+++ API-Controle-Estoque/Controllers/ProdutosController.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace API_Controle_Estoque.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly ILogger<ProdutosController> _logger;
+
+        public ProdutosController(ILogger<ProdutosController> logger)
+        {
+            _logger = logger;
+        }
+
+        public static List<ProdutosModel> PRODUTOS = new();
+
+
+        [HttpGet]
+        [SwaggerOperation(Summary = "Recupera uma lista de produtos")]
+        public async Task<ActionResult> Get()
+        {
+            return Ok(PRODUTOS);
+        }
+
+        [HttpPost]
+        [SwaggerOperation(Summary = "Inseri um produto")]
+        public async Task<ActionResult> Post(ProdutosModel produto)
+        {
+            PRODUTOS.Add(produto);
+            return Ok(PRODUTOS);
+        }
+    }
+}
---
+++ Domain/Models/ProdutosModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Models
+{
+    public class ProdutosModel
+    {
+        public ProdutosModel(string nome, string descricao)
+        {
+            Nome = nome;
+            Descricao = descricao;
+        }
+
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+    }
+}
